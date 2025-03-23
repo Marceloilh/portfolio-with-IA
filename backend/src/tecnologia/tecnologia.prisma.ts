f8d522ad117a899258211be9a741080f8d0d5803
Almeida <marceloalmeidacrm@gmail.com>
2025-03-23 19:16:05 -0300

    feat(core)

--- backend/src/tecnologia/tecnologia.prisma.ts
+++ backend/src/tecnologia/tecnologia.prisma.ts
@@ -3,10 +3,18 @@ import { PrismaProvider } from "src/db/prisma.provider"
 import { Tecnologia } from "@core"
 
 @Injectable()
-export class TecnologiaProvider {
+export class TecnologiaPrisma {
     constructor(private readonly prisma: PrismaProvider) {}
 
     async obterTodas(): Promise<Tecnologia[]> {
         return this.prisma.tecnologia.findMany()
+    }
+
+    async obterDestaques(): Promise<Tecnologia[]> {
+        return this.prisma.tecnologia.findMany({
+            where: {
+                destaque: true,
+            },
+        })
     }
 }
